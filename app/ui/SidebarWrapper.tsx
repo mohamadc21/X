50cd963d1144d2e46d86dc5fd0b7e90d378eec1b
mohamad-21 <wyattmohammad1371017@gmail.com>
2024-10-01 01:17:14 +0330

    update

--- app/ui/SidebarWrapper.tsx
+++ app/ui/SidebarWrapper.tsx
@@ -6,7 +6,7 @@ import Navigation from "./Navigation";
 
 function SidebarWrapper() {
   return (
-    <div className="sm:h-[98dvh] flex sm:flex-col justify-between items-center w-full xl:max-w-[300px] sm:max-w-[85px] max-w-full overflow-hidden text-foreground py-3 gap-12 flex-1 sm:sticky sm:top-0 left-0 bg-background fixed bottom-0 sm:border-t-0 border-t border-default z-[2]">
+    <div className="sm:h-[98dvh] flex sm:flex-col justify-between items-center w-full xl:max-w-[300px] sm:max-w-[85px] max-w-full overflow-hidden text-foreground py-3 gap-12 flex-1 sm:sticky sm:top-0 left-0 bg-background fixed bottom-0 sm:border-t-0 border-t border-default z-[5]">
       <Suspense fallback={<LoadingSpinner noPadding />}>
         <Sidebar />
       </Suspense>
