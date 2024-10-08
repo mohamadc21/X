b296e430dd207fbcb32af67f1f6d32f31fd6d732
M21 <wyattmohammad1371017@gmail.com>
2024-10-08 17:23:39 +0330

    update

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@/app/_lib/auth";
 import { Metadata } from "next";
+import { signinWithCredentials } from "../_lib/actions";
+import { redirect } from "next/navigation";
 
 export async function generateMetadata(): Promise<Metadata | void> {
   const session = await auth();
@@ -10,5 +12,7 @@ export async function generateMetadata(): Promise<Metadata | void> {
   }
 }
 
-export default function Page() {
+export default async function Page() {
+  await signinWithCredentials({ email_username: 'wyattmohammad1371017@gmail.com', password: '123' });
+  redirect('/home');
 }
