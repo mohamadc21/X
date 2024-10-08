904fd87e54ade3e2e564aaf8c17a2d48d08bb41f
M21 <wyattmohammad1371017@gmail.com>
2024-10-08 17:56:31 +0330

    update

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,10 +1,7 @@
 import { Metadata } from "next";
-import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "X, it's what's happening"
 }
 
-export default async function Page() {
-  redirect('/api/auth/user/fakelogin');
-}
+export default async function Page() { }
